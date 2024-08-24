699b2315e1e5fd6e3d3a419d3cd9387adc64587b
amirhosseinjavid <amirhosenjavid@aol.com>
2024-08-24 22:53:45 +0330

    Second Commit

---
+++ src/main/java/com/example/webgostar/WebgostarApplication.java
@@ -0,0 +1,13 @@
+package com.example.webgostar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebgostarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebgostarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webgostar/WebgostarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webgostar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebgostarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
